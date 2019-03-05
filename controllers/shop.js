5db4552acbe616869431dceaa2c238ad5758d957
Julian Estefan <julian.y.estefan@gmail.com>
2019-03-05 01:55:24 -0300

    Ajustado responsive. Agregada vista y controlador de detalles de producto. Agregado metodo en el modelo de producto para obtener un producto

--- controllers/shop.js
+++ controllers/shop.js
@@ -19,3 +19,16 @@ exports.getProducts = (req, res, next) => {
         });
     });
 };
+
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if (product) {
+            res.render('shop/product-detail', {
+                product: product,
+                pageTitle: product.title,
+                path: '/products'
+            });
+        }
+    });
+};
--- routes/shop.js
+++ routes/shop.js
@@ -8,4 +8,6 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+
 module.exports = router;
