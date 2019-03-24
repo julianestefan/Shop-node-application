cb973653ed77462329fa2c4946f0ba3186841596
Julian Estefan <julian.y.estefan@gmail.com>
2019-03-24 12:08:38 -0300

    Added structure to allow login operations.

---
+++ controllers/auth.js
@@ -0,0 +1,9 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
+};
