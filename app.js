dec5d306a135d93d74f541e4fb03d70b31bec8f4
Julian Estefan <julian.y.estefan@gmail.com>
2019-03-25 21:00:52 -0300

    Refactored routes to configure all with a single function in the app.js file. Added route to login operations.

--- app.js
+++ app.js
@@ -5,36 +5,19 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const User = require('./models/user');
 const connectDB = require('./config/db');
-const errorController = require('./controllers/error');
+const configureSession = require('./config/session');
+const configureRoutes = require('./routes/routes');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const shopRoutes = require('./routes/shop');
-const adminRoutes = require('./routes/admin');
-const authRoutes = require('./routes/auth');
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-  User.findById('5c969f232211f225484d089f')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
-
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-app.use(authRoutes);
-
-app.use(errorController.get404);
+configureSession(app);
+configureRoutes(app);
 
 connectDB(() => { app.listen(3000); })
 
---
+++ routes/routes.js
@@ -0,0 +1,18 @@
+"use strict";
+const bodyParser = require('body-parser');
+
+const shopRoutes = require('./shop');
+const adminRoutes = require('./admin');
+const authRoutes = require('./auth');
+const errorController = require('../controllers/error');
+
+const configureRoutes = (app) => {
+    app.use(bodyParser.urlencoded({ extended: false }));
+
+    app.use('/admin', adminRoutes);
+    app.use(shopRoutes);
+    app.use(authRoutes);
+    app.use(errorController.get404);
+}
+
+module.exports= configureRoutes;
